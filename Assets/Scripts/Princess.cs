0f98776c38946c1fc5dd31e83728a3dcf8afc108
Marvin <marvin.k.scharding@gmail.com>
2020-02-22 16:54:37 +0100

    added princess script wiht abylity to move around the place

--- Assets/Scripts/Princess.cs
+++ Assets/Scripts/Princess.cs
@@ -5,21 +5,85 @@ using UnityEngine;
 public class Princess : MonoBehaviour
 {
     Vector2 position;
-
+    [SerializeField] float speed;
+    Vector2 basePosition;
+    [SerializeField] float movingAroundTimer;
+    float movingAroundTime;
+    float xTarget;
+    float yTarget;
+    Vector2 newTarget;
+    bool choseNewTarget = false;
+   [SerializeField] bool moveAround = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        basePosition = transform.position;
     }
+
+    enum State
+    {
+        NOT_MOVING,
+        MOVING_AROUND,
+        RETURNING_TO_BASE_LOCATION
+    }
+    State state = State.NOT_MOVING;
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(state)
+        {
+            case State.NOT_MOVING:
+                movingAroundTime = movingAroundTimer;
+                choseNewTarget = true;
+                if(moveAround)
+                {
+                    state = State.MOVING_AROUND;
+                }
+                break;
+            case State.MOVING_AROUND:
+                movingAroundTime -= Time.deltaTime;
+                if(choseNewTarget)
+                {
+                randomTarget();
+                }
+                if (Vector2.Distance(newTarget, transform.position) > 0.1f)
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, newTarget, speed * Time.deltaTime);
+                }
+                if (Vector2.Distance(newTarget, transform.position) < 0.1f)
+                {
+                    choseNewTarget = true;
+                    randomTarget();
+                }
+                if(movingAroundTime<=0)
+                {
+                    state = State.RETURNING_TO_BASE_LOCATION;
+                }
+                break;
+            case State.RETURNING_TO_BASE_LOCATION:
+                if (Vector2.Distance(basePosition, transform.position) > 0.1f)
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, basePosition, speed * Time.deltaTime);
+                }
+                if (Vector2.Distance(basePosition, transform.position) < 0.1f)
+                {
+                    state = State.NOT_MOVING;
+                }
+                moveAround = false;
+                break;
+        }
     }
     public Vector2 GivePosition()
     {
         position = new Vector2(transform.position.x, transform.position.y);
         return position;
+    }
+    void randomTarget()
+    {
+        xTarget = Random.Range(-4.18f, 4.46f);
+        yTarget = Random.Range(3.72f, -4.65f);
+        newTarget = new Vector2(xTarget, yTarget);
+        choseNewTarget = false;
     }
 }
